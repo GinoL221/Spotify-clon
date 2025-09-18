3bee77a82554caae295e9245ab64253c9ffae1ea
Gino Lencina <ginolencina@gmail.com>
2025-09-18 11:06:50 -0300

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,js,jsx,ts,tsx,vue}", "./public/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
