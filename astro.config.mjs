f3b61fb3894cb36da2fbc5cc30335187079dcd56
Gino Lencina <ginolencina@gmail.com>
2025-09-19 21:03:58 -0300

    feat: agregar alias de rutas para componentes y servicios en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import path from "path";
-
 import tailwindcss from "@tailwindcss/vite";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify/functions";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     resolve: {
       alias: {
         "@": path.resolve("./src"),
+        "@/components": path.resolve("./src/components"),
+        "@/hooks": path.resolve("./src/hooks"),
+        "@/icons": path.resolve("./src/icons"),
+        "@/lib": path.resolve("./src/lib"),
+        "@/services": path.resolve("./src/services"),
       },
     },
   },
